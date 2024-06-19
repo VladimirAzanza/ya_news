e3e3215f95a95bb140c87c6b32f708dd17ed7402
Vladimir Azanza <vladimirazanza@gmail.com>
2024-06-19 14:56:51 +0500

    Add test for non authenthicated users to test routes

--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -11,14 +11,16 @@ class TestRoutes(TestCase):
     def setUpTestData(cls):
         cls.news = News.objects.create(title='Заголовок', text='Текст')
 
-    def test_home_page(self):
-        '''Главная страница доступна анонимному пользователю.'''
-        url = reverse('news:home')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-    
-    def test_detail_page(self):
-        '''Страница отдельной новости доступна анонимному пользователю.'''
-        url = reverse('news:detail', kwargs={'pk': self.news.id})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
