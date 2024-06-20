58527206a2494d55397605084a2c88240f3efa58
Vladimir Azanza <vladimirazanza@gmail.com>
2024-06-20 15:35:57 +0500

    Add test for comments order

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timedelta
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from django.utils import timezone
 
-from news.models import News
+from news.models import Comment, News
+
+User = get_user_model()
 
 
-class TestContent(TestCase):
+class TestHomePage(TestCase):
     HOME_URL = reverse('news:home')
 
     @classmethod
@@ -34,3 +38,29 @@ class TestContent(TestCase):
         all_dates = [news.date for news in object_list]
         sorted_dates = sorted(all_dates, reverse=True)
         self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        now = timezone.now()
+        for index in range(10):
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            comment.created = now + timedelta(days=index)
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        self.assertIn('news', response.context)
+        news = response.context['news']
+        all_comments = news.comment_set.all()
+        all_timestamps = [comment.created for comment in all_comments]
+        sorted_timestamps = sorted(all_timestamps)
+        self.assertEqual(all_timestamps, sorted_timestamps)
