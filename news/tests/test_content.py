a64e251030a1ca569fe058e3b6d1a162af0e6bae
Vladimir Azanza <vladimirazanza@gmail.com>
2024-06-20 14:35:43 +0500

    Add test for ordering publications by date

--- news/tests/test_content.py
+++ news/tests/test_content.py
@@ -8,7 +8,6 @@ from news.models import News
 
 
 class TestContent(TestCase):
-
     HOME_URL = reverse('news:home')
 
     @classmethod
@@ -17,7 +16,7 @@ class TestContent(TestCase):
             News(
                 title=f'Новость {index}',
                 text='Просто текст.',
-                date=datetime.today()- timedelta(days=index)
+                date=datetime.today() - timedelta(days=index)
             )
             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
         )
@@ -28,3 +27,10 @@ class TestContent(TestCase):
         object_list = response.context['object_list']
         news_count = object_list.count()
         self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        all_dates = [news.date for news in object_list]
+        sorted_dates = sorted(all_dates, reverse=True)
+        self.assertEqual(all_dates, sorted_dates)
